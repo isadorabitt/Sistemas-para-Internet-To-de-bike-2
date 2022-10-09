18160913d2380da6d527f3b52b9b85b67333a8c2
Maria Eduarda Almeida <mariaedealmeida7@gmail.com>
2022-10-09 16:09:15 -0300

    nova branch

--- bike-landing/src/App.js
+++ bike-landing/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Menu } from "./components/Menu";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./GlobalStyles";
 
--- bike-landing/src/pages/Home.js
+++ bike-landing/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Hero } from '../components/Hero'
 
+
 export const Home = () => {
   return (
     <Hero />
