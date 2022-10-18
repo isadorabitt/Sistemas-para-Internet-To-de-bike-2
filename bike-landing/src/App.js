a99c9705c57c6acbf7cfe80a680932a161415fe2
Maria Eduarda Almeida <mariaedealmeida7@gmail.com>
2022-10-18 18:20:03 -0300

    merge main-develop

--- bike-landing/src/App.js
+++ bike-landing/src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { GlobalStyle } from "./GlobalStyles";
-<<<<<<< HEAD
 import { Menu } from "./pages/Menu";
 import { Station } from "./pages/Station";
 import { Wallet } from "./pages/Wallet";
 import { Bikes } from "./pages/Bikes";
 import { Help } from "./pages/Help";
-=======
 import {Register} from "./pages/Register";
 import {Login} from "./pages/Login";
->>>>>>> development
 
 function App() {
   return (
@@ -19,17 +16,13 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/menu/estacoes" element={<Station />} />
         <Route path="/menu/carteira" element={<Wallet />} />
         <Route path="/menu/bikes" element={<Bikes />} />
         <Route path="/menu/Ajuda" element={<Help />} />
-=======
         <Route path="/cadastro" element={<Register />} />
         <Route path="/login" element={<Login />} />
->>>>>>> development
       </Routes>
     </BrowserRouter>
   );
